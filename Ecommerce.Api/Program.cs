9b04463bf08daeb11c8fa35c3869818193ec0679
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-10-19 16:33:42 +0300

    [Add] ConnectionString, Migration Files, ApplicationDbContext

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,3 +1,6 @@
+using Ecommerce.Infrastracture.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+//Add Connection String
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("TestConnection")));
+
+
+
+
 
 var app = builder.Build();
 
---
+++ Ecommerce.Infrastracture/Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastracture.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+            
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
+    }
+}
