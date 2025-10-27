2333612fda68e014eb16368cbdd159170c8dc49b
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-10-27 16:37:10 +0300

    [Add] Profile Mapping as a partial class and Entity Mapping as a partial class with void method

---
+++ Ecommerce.Core/Mapping/Profiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ecommerce.Core.Product.Queries.Results;
+using Ecommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Mapping
+{
+    public partial class Profiles : Profile
+    {
+        public Profiles()
+        {
+            ConfigureProductMapping();
+        }
+    }
+}
---
+++ Ecommerce.Core/Product/ProductMapping.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Core.Product.Queries.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Core.Mapping
+{
+    public partial class Profiles
+    {
+        public void ConfigureProductMapping()
+        {
+            CreateMap<Ecommerce.Domain.Models.Product, GetAllProductsResponse>()
+                    .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name))
+                    .ForMember(dest => dest.Stock, opt => opt.MapFrom(src => src.StockQuantity));
+        }
+    }
+}
