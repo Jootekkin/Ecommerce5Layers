7aec4af688db299c99d6f027a1341aeac28220ac
Yusuf Mostafa <yusufmustafa1@outlook.com>
2025-11-05 16:19:23 +0200

    [Add] Global middleware

---
+++ Ecommerce.Core/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.Core.Middleware
+{
+    public class GlobalExceptionMiddleware
+    {
+        #region Fields
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        #endregion
+
+        #region Constructors
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        #endregion
+
+        #region Methods
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception has occurred while processing the request.");
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+                var response = new { Message = $"{ex.Message}" };
+                await context.Response.WriteAsync(System.Text.Json.JsonSerializer.Serialize(response));
+            }
+        }
+        #endregion
+    }
+}
+
+
